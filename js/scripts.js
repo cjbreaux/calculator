cbb863b552436507e79d3576067f64d87c3869b1
Chris Breaux and Tate Easterbrooks <chris@>
2019-01-14 13:39:55 -0800

    add addition script to js file

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 =parseInt(prompt("Pick a number, any number!"));
+var number2 =parseInt(prompt("Pick another number."));
+alert(add(number1, number2));
