e16a56559bf9762a2c0f169bd081ceff8614f005
Chris Breaux and Tate Easterbrooks <tate@>
2019-01-14 13:45:19 -0800

    add multiply, divide, and subtract functions to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,27 @@ var add = function(number1, number2) {
 var number1 =parseInt(prompt("Pick a number, any number!"));
 var number2 =parseInt(prompt("Pick another number."));
 alert(add(number1, number2));
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 =parseInt(prompt("Pick a number, any number!"));
+var number2 =parseInt(prompt("Pick another number."));
+alert(subtract(number1, number2));
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 =parseInt(prompt("Pick a number, any number!"));
+var number2 =parseInt(prompt("Pick another number."));
+alert(multiply(number1, number2));
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 =parseInt(prompt("Pick a number, any number!"));
+var number2 =parseInt(prompt("Pick another number."));
+alert(divide(number1, number2));
