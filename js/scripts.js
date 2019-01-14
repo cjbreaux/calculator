7e211e3e3c5ebf203d17753221d6ea047b081593
Chris Breaux and Tate Easterbrooks <tate@>
2019-01-14 14:59:06 -0800

    add more conversions i comments

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,20 @@ var divide = function(number1, number2) {
 var number1 =parseInt(prompt("Pick a number, any number!"));
 var number2 =parseInt(prompt("Pick another number."));
 alert(divide(number1, number2));
+
+
+// function BMI (height,weight) {
+// 	return weight / (height^2);
+// };
+//
+// var weight =parseInt(prompt("What is your weight in kilos?"));
+// var height =parseInt(prompt("What is your height in meters?"));
+// alert(BMI(height, weight));
+
+
+// function Temp(Celsius) {
+// return (Celsius * 9/5) + 32;
+// };
+//
+// var Celsius = parseInt(prompt("Temperature in Celsius"));
+// alert("Temperature in Farenheit " + Temp(Celsius));
