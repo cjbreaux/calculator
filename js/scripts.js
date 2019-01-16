9c69814308e7769498d69a538a3c1e427666e814
Chris Breaux and Tien Le <chris@>
2019-01-16 14:12:20 -0800

    Add forms, refactor function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var add = function(number1, number2) {
+function add(number1, number2) {
   return number1 + number2;
 };
 
@@ -6,29 +6,29 @@ var number1 =parseInt(prompt("Pick a number, any number!"));
 var number2 =parseInt(prompt("Pick another number."));
 alert(add(number1, number2));
 
-var subtract = function(number1, number2) {
+function subtract(number1, number2) {
   return number1 - number2;
 };
-
-var number1 =parseInt(prompt("Pick a number, any number!"));
-var number2 =parseInt(prompt("Pick another number."));
-alert(subtract(number1, number2));
-
-var multiply = function(number1, number2) {
+//
+// var number1 =parseInt(prompt("Pick a number, any number!"));
+// var number2 =parseInt(prompt("Pick another number."));
+// alert(subtract(number1, number2));
+//
+function multiply(number1, number2) {
   return number1 * number2;
 };
-
-var number1 =parseInt(prompt("Pick a number, any number!"));
-var number2 =parseInt(prompt("Pick another number."));
-alert(multiply(number1, number2));
-
-var divide = function(number1, number2) {
+//
+// var number1 =parseInt(prompt("Pick a number, any number!"));
+// var number2 =parseInt(prompt("Pick another number."));
+// alert(multiply(number1, number2));
+//
+function divide(number1, number2) {
   return number1 / number2;
-};
-
-var number1 =parseInt(prompt("Pick a number, any number!"));
-var number2 =parseInt(prompt("Pick another number."));
-alert(divide(number1, number2));
+// };
+//
+// var number1 =parseInt(prompt("Pick a number, any number!"));
+// var number2 =parseInt(prompt("Pick another number."));
+// alert(divide(number1, number2));
 
 
 // function BMI (height,weight) {
